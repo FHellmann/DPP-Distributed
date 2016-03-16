68648e44f9b7c5fdb311e89e8b1b4f04cf805e69
FHellmann <fhellmann93@googlemail.com>
2016-03-16 11:08:42 +0100

    Initial Commit

---
+++ src/test/java/edu/hm/cs/vss/SampleTest.java
@@ -0,0 +1,14 @@
+package edu.hm.cs.vss;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Fabio Hellmann on 16.03.2016.
+ */
+public class SampleTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(4 + 2 == 6);
+    }
+}
